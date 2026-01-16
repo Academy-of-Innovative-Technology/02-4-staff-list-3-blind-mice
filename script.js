e9d6863df3aa2680070633b59ae5542137e04c69
Dafne <dafnej@nycstudents.net>
2026-01-16 13:58:42 -0500

    completed

--- script.js
+++ script.js
@@ -16,12 +16,76 @@ let
 	Support = ["Kabir", "Minto"],
 	SSC = ["Stephine"];
 
-const STAFF = [Admin, Office, Counselor, CTE, History, Maths, Gym, Dean, Language, Science, English, Support, SSC];
-
 //////// DO NOT EDIT ABOVE THIS LINE ////////////////
 /////////////////////////////////////////////////////
 
 ////////// MAKE YOUR UPDATES WITHIN THIS SECTION //// 
+Admin.splice(3);
+Admin.shift();
+Admin.shift();
+Admin.push("Drysdale","Simpkins");
+
+Office.shift();
+Office.push("Martinez", "Tirado", "Valentin", "Swing");
+
+Counselor.splice(0);
+Counselor.push("Molina");
+
+let Guidance = [];
+Guidance.push("Cooper", "Desroches");
+
+CTE.splice(1,4);
+CTE.pop();
+CTE.push("Frazer", "Pelzer", "Pierre", "Ramsawak", "Gomez", "Rahimi", "Sullivan" 
+);
+
+History.splice(0,1);
+History.splice(1,1);
+History.splice(1,1);
+History.push("Urena");
+
+Maths.pop();
+Maths.push("Koelsch", "Minto", "Forth", "Wong");
+
+Gym.splice(1);
+Gym.push("Mcalary");
+
+Dean.splice(1,1);
+
+Language.splice(0,5);
+Language.push("Nowakowski", "Vallejo");
+
+Science.splice(0);
+Science.push("Davies", "Lynch", "Simpkins", "Boswell");
+
+English.splice(4);
+English.splice(0,3);
+English.push("Grant", "Gomez", "Tavares");
+
+Support.push("Slater", "Sherman", "Rodriguez", "Steele", "David", "Howell", "Valentin", "Jorge", "Belgrave");
+
+let Paraprofessional = [];
+Paraprofessional.push("Shephard", "Ward", "Alabsi", "Colter", "Camila", "Shand", "Ahad", "Nicholson", "J. Grant", "Belton", "Damari");
+
+SSC.splice(0);
+SSC.push("Guzman");
+
+let Interns = [];
+Interns.push("Krishe", "Rachael")
+
+Admin.sort();
+CTE.sort();
+History.sort();
+Counselor.sort();
+Maths.sort();
+Gym.sort();
+Dean.sort();
+Language.sort();
+Science.sort();
+English.sort();
+Office.sort();
+Support.sort();
+SSC.sort();
 
 
 
@@ -68,6 +132,6 @@ const STAFF = [Admin, Office, Counselor, CTE, History, Maths, Gym, Dean, Languag
 //////// DO NOT EDIT BELOW THIS LINE ////////////////
 /////////////////////////////////////////////////////
 
-
+const STAFF = [Admin, Office, Counselor, CTE, History, Maths, Gym, Dean, Language, Science, English, Support, SSC];
 
 e("table").innerHTML = createTableCotents();
